34c202f4e90ec7673cff8c25fe47a49544fa2aa8
DaveWestFGCU <dawest6674@eagle.fgcu.edu>
2023-05-01 09:21:52 -0400

    Added to King class: - validMove(Location) - validAttack(Location)

--- King.h
+++ King.h
@@ -1,7 +1,7 @@
 #ifndef CHECKERS_KING_H
 #define CHECKERS_KING_H
 
-#include "Man.h"
+#include "Piece.h"
 
 class King : public Man {
 
@@ -11,30 +11,39 @@ public:
     ~King() { std::cout << "King destructor called." << std::endl;  }
 
     bool validMove(Location newLoc);
+    bool validAttack(Location newLoc);
 };
 
 
 /**
- * Determines if the piece is moving as it should
+ * Basic movement, determine whether move is valid
  * @param newX int -> new x coordinate
  * @param newY int -> new y coordinate
  * @return bool -> true if the move is valid
  */
 bool King::validMove(Location newLoc) {
-    // Basic movement
     // Check if move is one space to left or right
-    if (newLoc.getX() == myLocation.getX() + 1 || newLoc.getX() == myLocation.getX() - 1 ) {
-
-        // Check if move is "forward", which depends on player
-        if (player == 0 && newLoc.getY() == myLocation.getY() + 1)
+    if (newLoc.getX() == myLocation.getX() + 1 || newLoc.getX() == myLocation.getX() - 1 )
+        // Check if move is one space forward or back
+        if ( newLoc.getX() == myLocation.getY() + 1 || newLoc.getX() == myLocation.getY() - 1 )
             return true;
-
-        if (player == 1 && newLoc.getY() == myLocation.getY() - 1)
-            return true;
-    }
 
     return false;
 }   // end validMove()
 
+/**
+ * Determines if a move attempt is a valid attacking move (moving 2 spots)
+ * @param newLoc
+ * @return
+ */
+bool King::validAttack(Location newLoc) {
+    // Jump movement
+    // Check if move is two spaces to left or right
+    if ( newLoc.getX() == myLocation.getX() + 2 || newLoc.getX() == myLocation.getX() - 2 )
+        // Check if move is two spaces forward or back
+        if ( newLoc.getX() == myLocation.getY() + 2 || newLoc.getX() == myLocation.getY() - 2 )
+            return true;
 
+    return false;
+}
 #endif //CHECKERS_KING_H
